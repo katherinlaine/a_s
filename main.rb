6da37bf85e8a0901a52f4e4f52e13f1653db2b40
kaffin <katherin.siracusa@gmail.com>
2014-12-30 11:25:42 -0500

    set up basic structure for assessing format

--- main.rb
+++ main.rb
@@ -11,15 +11,20 @@ class Main
     }
   end
 
-  def collect_domains
+  def collect_known_domains
     @data.each do |name, email|
       domain = email.split('@').last
       unless @known_domains.include? domain
         @known_domains << domain
       end
     end
-    binding.pry
   end
+
+  def enter_query
+    get_user_input
+  end
+
+  private
 
   def get_user_input
     print "First Name: "
@@ -28,6 +33,56 @@ class Main
     @last_name = gets.chomp
     print "Domain: "
     @domain = gets.chomp
+  end
+
+
+  def first_is_initial?
+    @names[0].length == 1
+  end
+
+  def last_is_initial?
+    @names[1].length == 1
+  end
+
+  def assess_format
+    if first_is_initial? && last_is_initial?
+      puts "initial initial"
+      first_initial_dot_last_initial
+    elsif first_is_initial?
+      puts "initial name"
+      first_initial_dot_last_name
+    elsif last_is_initial?
+      puts "name initial"
+      first_name_dot_last_initial
+    else
+      puts "name name"
+      first_name_dot_last_name
+    end
+  end
+
+  def first_initial_dot_last_initial
+    @first = @first_name[0]
+    @last = @last_name[0]
+  end
+
+  def first_initial_dot_last_name
+    @first = @first_name[0]
+    @last = @last_name
+  end
+
+  def first_name_dot_last_initial
+    @first = @first_name
+    @last = @last_name[0]
+  end
+
+  def first_name_dot_last_name
+    @first = @first_name
+    @last = @last_name
+  end
+
+  def assemble
+    full_address = @first + "." + last + '@' + @domain
+    puts full_address
   end
 
 end
