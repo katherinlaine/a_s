dd7a5732b6612b45c6d0c54bfd1fefde85ff3d3f
kaffin <katherin.siracusa@gmail.com>
2014-12-30 15:25:57 -0500

    remove data into yml file

--- data.rb
+++ data.rb
@@ -1,16 +1,11 @@
-class DataCollector
+require "yaml"
+
+class DataParser
   attr_reader :data, :known_domains
 
   def initialize
     @known_domains = {}
-    @data = {
-      "John Ferguson" => "john.ferguson@alphasights.com",
-      "Damon Aw" => "damon.aw@alphasights.com",
-      "Linda Li" => "linda.li@alphasights.com",
-      "Larry Page" => "larry.p@google.com",
-      "Sergey Brin" => "s.brin@google.com",
-      "Steve Jobs" => "s.j@apple.com",
-    }
+    @data = YAML.load_file('addresses.yml')
     collect_known_domains
   end
 
--- main.rb
+++ main.rb
@@ -1,35 +1,18 @@
+require "./data"
+
 class Main
   def initialize
-    @known_domains = {}
-    @data = {
-      "John Ferguson" => "john.ferguson@alphasights.com",
-      "Damon Aw" => "damon.aw@alphasights.com",
-      "Linda Li" => "linda.li@alphasights.com",
-      "Larry Page" => "larry.p@google.com",
-      "Sergey Brin" => "s.brin@google.com",
-      "Steve Jobs" => "s.j@apple.com"
-    }
-  end
-
-  def collect_known_domains
-    @data.each do |name, email|
-      address = email.split('@').first
-      @names = address.split('.')
-      domain = email.split('@').last
-      assess_format
-      if @known_domains.has_key? domain
-        if @known_domains[domain] != @format
-          @known_domains[domain] = "Conflicting Data"
-        end
-      else
-        @known_domains[domain] = @format
-      end
-    end
-    binding.pry
+    @data = DataParser.new
   end
 
   def enter_query
     get_user_input
+    if domain_is_known?
+      set_up_formats
+      assemble
+    else
+      puts "I'm sorry, we don't have any records with that domain."
+    end
   end
 
   private
@@ -43,54 +26,32 @@ class Main
     @domain = gets.chomp
   end
 
-
-  def first_is_initial?
-    @names[0].length == 1
+  def domain_is_known?
+    @data.known_domains.has_key? @domain
   end
 
-  def last_is_initial?
-    @names[1].length == 1
-  end
-
-  def assess_format
-    if first_is_initial? && last_is_initial?
-      @format = :first_initial_dot_last_initial
-    elsif first_is_initial?
-      @format = :first_initial_dot_last_name
-    elsif last_is_initial?
-      @format = :first_name_dot_last_initial
-    else
-      @format = :first_name_dot_last_name
-    end
-  end
-
-  def first_initial_dot_last_initial
-    @first = @first_name[0]
-    @last = @last_name[0]
-  end
-
-  def first_initial_dot_last_name
-    @first = @first_name[0]
-    @last = @last_name
-  end
-
-  def first_name_dot_last_initial
-    @first = @first_name
-    @last = @last_name[0]
-  end
-
-  def first_name_dot_last_name
-    @first = @first_name
-    @last = @last_name
+  def set_up_formats
+    @possible_formats = {
+      first_name_dot_last_name: [@first_name, @last_name],
+      first_initial_dot_last_initial: [@first_name[0], @last_name[0]],
+      first_initial_dot_last_name: [@first_name, @last_name[0]],
+      first_name_dot_last_initial: [@first_name[0], @last_name]
+    }
   end
 
   def assemble
-    full_address = @first + "." + last + '@' + @domain
-    puts full_address
+    if @data.known_domains[@domain] == "Conflicting Data"
+      puts "Unable to determine format"
+    else
+      format = @data.known_domains[@domain]
+      formatting = @possible_formats[format]
+      first = formatting[0]
+      last = formatting[1]
+      full_address = first + "." + last + '@' + @domain
+      puts full_address
+    end
   end
-
 end
 
 round = Main.new
-round.collect_known_domains
 round.enter_query
